b6815d38ccff0b67a8d6e734f901bbc525907a55
DioverDM <97247851+dioverdm@users.noreply.github.com>
2025-03-16 02:24:32 -0400

    Update footerData.js

--- src/data/footerData.js
+++ src/data/footerData.js
@@ -1,4 +1,4 @@
-import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+import { FaFacebookF, FaInstagram, FaTiktok } from 'react-icons/fa';
 
 export const footMenu = [
     {
