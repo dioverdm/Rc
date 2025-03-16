41e9ebdf2fd1242a76be3e3d416eaeef3c9f4795
DioverDM <97247851+dioverdm@users.noreply.github.com>
2025-03-16 02:35:38 -0400

    Update productsData.js

--- src/data/productsData.js
+++ src/data/productsData.js
@@ -20,6 +20,27 @@ const productsData = [
         rateCount: 5,
         path: "/product-details/",
     },
+    {
+        id: 2,
+        tag: "featured-product",
+        tagline: "Keep the noise out, or in. You choose.",
+        heroImage: "/images/products/bujias1.png",
+        images: [
+            "/images/products/bujias1.png",
+        ],
+        brand: "Bujias Champion",
+        title: "Bujias Bl-15y Champion 350-305 /302-351 Motor V8 V6 Blazer",
+        info: "Modelo: RC12YC, Rango térmico: 6",
+        category: "Headphones",
+        type: "Champions",
+        connectivity: "14mm",
+        finalPrice: 14999,
+        originalPrice: 14999,
+        quantity: 1,
+        ratings: 1234,
+        rateCount: 5,
+        path: "/product-details/",
+    },
 ];
 
 export default productsData;
