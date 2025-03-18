3d9762f37cd171e11eecacbce97d1121cb8164c2
DioverDM <97247851+dioverdm@users.noreply.github.com>
2025-03-17 21:43:21 -0400

    Create appInstall.js

---
+++ src/components/common/appInstall.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+
+function InstallPWA() {
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+  const [showInstallButton, setShowInstallButton] = useState(false);
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e) => {
+      e.preventDefault();
+      setDeferredPrompt(e);
+      setShowInstallButton(true);
+    };
+
+    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+    };
+  }, []);
+
+  const handleInstallClick = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('PWA installed');
+        } else {
+          console.log('PWA installation rejected');
+        }
+        setDeferredPrompt(null);
+        setShowInstallButton(false);
+      });
+    }
+  };
+
+  return (
+    <div>
+      {showInstallButton && (
+        <button onClick={handleInstallClick}>Instalar la aplicación</button>
+      )}
+    </div>
+  );
+}
+
+export default InstallPWA;
