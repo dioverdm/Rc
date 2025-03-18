ab0c2f239283d164cc77428053ec334151c34e0b
DioverDM <97247851+dioverdm@users.noreply.github.com>
2025-03-17 22:45:48 -0400

    Update InstallSection.js

--- src/components/sliders/InstallSection.js
+++ src/components/sliders/InstallSection.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import InstallPWA from '../common/appInstall';
+import InstallPWA from '../common/appInstall'; // Asegúrate de que la ruta sea correcta
 
 function InstallSection() {
   const [showInstallSection, setShowInstallSection] = useState(false);
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
 
   // Función para verificar si la PWA ya está instalada
   const isAppInstalled = () => {
@@ -22,7 +23,8 @@ function InstallSection() {
 
     const handleBeforeInstallPrompt = (e) => {
       e.preventDefault();
-      console.log('Evento beforeinstallprompt disparado'); // Verifica en la consola
+      console.log('Evento beforeinstallprompt capturado en InstallSection'); // Verifica en la consola
+      setDeferredPrompt(e);
       setShowInstallSection(true); // Mostrar la sección si el navegador es compatible
     };
 
@@ -33,13 +35,28 @@ function InstallSection() {
     };
   }, []);
 
+  const handleInstallClick = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('PWA instalada');
+        } else {
+          console.log('Instalación de PWA rechazada');
+        }
+        setDeferredPrompt(null);
+        setShowInstallSection(false); // Ocultar la sección después de la instalación
+      });
+    }
+  };
+
   return (
     <>
       {showInstallSection && (
         <div className="install_section" style={{ textAlign: 'center', padding: '20px', backgroundColor: '#f5f5f5', margin: '20px 0' }}>
           <h2>¡Instala nuestra aplicación!</h2>
           <p>Disfruta de una experiencia más rápida y accede a nuestras funciones sin conexión.</p>
-          <InstallPWA />
+          <InstallPWA deferredPrompt={deferredPrompt} onInstallClick={handleInstallClick} />
         </div>
       )}
     </>
