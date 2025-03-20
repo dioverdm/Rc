827c18a4010fc722b266cea949c19194e7fbc0d1
DioverDM <97247851+dioverdm@users.noreply.github.com>
2025-03-19 20:15:07 -0400

    Update reviewsData.js

--- src/data/reviewsData.js
+++ src/data/reviewsData.js
@@ -1,38 +1,10 @@
 const reviewsData = [
     {
         id: 1,
-        name: "Atharva Kumar",
-        date: "4 Aug 2022",
-        review: "Sound is awesome and as I expected, love it.",
+        name: "Diover Rivero",
+        date: "19 Marzo 2025",
+        review: "Repuestos Caribe me ha demostrado su interes por sus clientes, felicidades muy buen trabajo.",
         rateCount: 5,
-    },
-    {
-        id: 2,
-        name: "Ritika Sen",
-        date: "15 July 2022",
-        review: "Very good and awesome product",
-        rateCount: 5,
-    },
-    {
-        id: 3,
-        name: "Bhavesh Joshi",
-        date: "10 June 2022",
-        review: "Super amazing product !!!",
-        rateCount: 4,
-    },
-    {
-        id: 4,
-        name: "Anandi Gupta",
-        date: "6 May 2022",
-        review: "Great NC, sound is a bit flat",
-        rateCount: 4,
-    },
-    {
-        id: 5,
-        name: "Arif Khan",
-        date: "27 April 2022",
-        review: "Very good but still has flaws!",
-        rateCount: 3,
     },
 ]
 
